03839785145b05777150dc17087cc03c6a5f63a8
hulloanson <pm@hulloanson.com>
2023-10-12 21:50:54 -0400

    fixed option not hiding entirely

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
+var HIDDEN_CLASS = "hidden";
 window.addEventListener("load", function () {
   var progressStore = new ProgressStore();
   var progress = progressStore.getProgress();
   if (progress) {
-    $("#resume-button").css("display", "");
-    $("#start-anew-text").css("display", "");
+    $("#resume").removeClass(HIDDEN_CLASS);
+    $("#start-anew-text").removeClass(HIDDEN_CLASS);
   }
 
   const store = new AnswersStore();
   const wronglyAnswered = store.getWronglyAnswered();
   if (wronglyAnswered.length !== 0) {
-    $("#review-wrong-button").css("display", "");
+    $("#review-wrong").removeClass(HIDDEN_CLASS);
   }
 });
